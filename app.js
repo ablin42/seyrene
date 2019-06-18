9cbce676f0d046f1499c093e42d3edfa4474e07d
ablin <space6fic@gmail.com>
2019-06-18 21:21:58 +0200

    update

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Parse app/json
 app.use(bodyParser.json());
 
-
+app.get('/', (res, req) => {
+    res.status(200).send('Hello world')
+})
 
 const port = process.env.PORT || 8089;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
