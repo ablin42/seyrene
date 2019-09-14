7293face81e0933e568f20dd6b091a763ae8ad2d
ablin <space6fic@gmail.com>
2019-09-14 02:51:02 +0200

    adding new pages, made footer and header partials, looking for design

--- app.js
+++ app.js
@@ -23,5 +23,33 @@ app.get('/', (req, res) => {
     res.status(200);
 })
 
+app.get('/Galerie', (req, res) => {
+    res.render('galerie', {
+        root: path.join(__dirname, '/pages/')
+    })
+    res.status(200);
+})
+
+app.get('/Login', (req, res) => {
+    res.render('login', {
+        root: path.join(__dirname, '/pages/')
+    })
+    res.status(200);
+})
+
+app.get('/Register', (req, res) => {
+    res.render('register', {
+        root: path.join(__dirname, '/pages/')
+    })
+    res.status(200);
+})
+
+app.get('/Blog', (req, res) => {
+    res.render('blog', {
+        root: path.join(__dirname, '/pages/')
+    })
+    res.status(200);
+})
+
 const port = process.env.PORT || 8089;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
