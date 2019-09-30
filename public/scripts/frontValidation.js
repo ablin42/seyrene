714ad2ef256e73c20ec39eb8beb46b9268987c1d
ablin <space6fic@gmail.com>
2019-10-01 00:25:06 +0200

    changing some text to english, fixed email front validation, form btn disabled when a field is invalid, renamed function for clarity

--- public/scripts/frontValidation.js
+++ public/scripts/frontValidation.js
@@ -1,11 +1,24 @@
+function getInvalid(formId) {
+    let submitId = "submit-" + formId,
+        submit = document.getElementById(submitId),
+        form = $(`#${formId}`),
+        invalids = form.find(".invalid");
+    
+    submit.removeAttribute("disabled")
+    if (invalids.length > 0)
+        submit.setAttribute("disabled", "")
+    return ;
+}
+
 let Validate = {
     String: function(item, min, max) {
         let inputId = item.id,
-        spanInfo = document.getElementById(`i_${inputId}`);
+            spanInfo = document.getElementById(`i_${inputId}`),
+            formId = item.form.id;
         
         if (item.value.length !== 0 && (item.value.length < min || item.value.length > max)) {
             spanInfo.style.display = "inline-block";
-            item.classList.add("invalid");
+            item.classList.add("invalid");    
         }
         else if (item.value.length >= min || item.value.length <= max) {
             spanInfo.style.display = "none";
@@ -17,21 +30,27 @@ let Validate = {
             item.classList.remove("invalid");
             item.classList.remove("valid");
         }
+        getInvalid(formId);
     },
     Email: function(email) {
         let inputId = email.id
-            spanInfo = document.getElementById(`i_${inputId}`);
+            spanInfo = document.getElementById(`i_${inputId}`),
+            formId = email.form.id;
 
         if (email.value.length !== 0 && (email.value.length < 3 || email.value.length > 256)){
             spanInfo.style.display = "inline-block";
             email.classList.add("invalid");
         }
         else if (email.value.length !== 0) {
+            console.log(email.value.length, "middle")
             if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value))
             {
                 spanInfo.style.display = "none";
                 email.classList.remove("invalid");
                 email.classList.add("valid");
+            } else {
+                spanInfo.style.display = "inline-block";
+                email.classList.add("invalid");
             }
         }
         else {
@@ -39,11 +58,13 @@ let Validate = {
             email.classList.remove("invalid");
             email.classList.remove("valid");
         }
+        getInvalid(formId);
     },
     Password: function(password) {
         let inputId = password.id,
             spanInfo = document.getElementById(`i_${inputId}`),
-            password2 = document.getElementById(`${inputId}2`);
+            password2 = document.getElementById(`${inputId}2`),
+            formId = password.form.id;
 
         if (password.value.length > 0 && (password.value.length > 256 || !/^(((?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(.{8,})/.test(password.value))) {
             spanInfo.style.display = "inline-block";
@@ -60,10 +81,12 @@ let Validate = {
             password.classList.remove("valid");
         }
         this.Password2(password2);
+        getInvalid(formId);
     },
     Password2: function(password2) {
         let inputId = password2.id,
-            spanInfo = document.getElementById(`i_${inputId}`);
+            spanInfo = document.getElementById(`i_${inputId}`),
+            formId = password2.form.id;
 
         if (password2.value.length !== 0 && (password.value !== password2.value)) {
             spanInfo.style.display = "inline-block";
@@ -79,5 +102,6 @@ let Validate = {
             password2.classList.remove("invalid");
             password2.classList.remove("valid");
         }
+        getInvalid(formId);
     }
 }
