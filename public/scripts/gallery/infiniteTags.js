0c8f47672ee5365978d8769a2202fd43b6ae8534
ablin <space6fic@gmail.com>
2020-01-11 04:50:50 +0100

    updating infinitetags to match with tags search html

--- public/scripts/gallery/infiniteTags.js
+++ public/scripts/gallery/infiniteTags.js
@@ -15,26 +15,32 @@ async function infiniteTags() {
         response.json().then(function(data) {
             if (!data.error) {
                 data.forEach(gallery => {
-                let id = gallery._id;
-                if ($(`#${id}`).length === 0) {
-                    let div = document.createElement('div');
-                    div.setAttribute("id", id);
-                    div.setAttribute("class", "card");
-                    div.setAttribute("style", "width: 18rem");
-                    let toAppend =  `
-                        <a href="#expand"><img onclick="expand(this);" src="/api/image/${gallery.mainImgId}" class="card-img-top" alt="${gallery.title}"></a>
-                        <div class="card-body">
-                            <h5 class="card-title"><i><a href="/Galerie/${id}">${gallery.title}</a></i></h5>
-                            <p class="card-text gallery-description">${gallery.content}</p>
-                            <p class="card-text">`;
-                    gallery.tags.forEach(tag => {
-                        toAppend += ` <a href="/Galerie/Tags?t=${tag}">#${tag}</a>`;
-                    })
-                    toAppend += `</p></div>`;
-                    div.innerHTML = toAppend;  
-                    id++;
-                    $("#container-gallery").append(div);
-                }});
+                    let id = gallery._id;
+                    if ($(`#${id}`).length === 0) {
+                        let div = document.createElement('div');
+                        div.setAttribute("class", "expandable-card");
+                        div.setAttribute("id", id);
+                        toAppend = `
+                        <div class="face face1">
+                            <a href="#expand">
+                                <img onclick="expand(this);" src="/api/image/${gallery.mainImgId}" class="card-img-top" alt="${gallery.shorttitle}">
+                            </a>
+                        </div>
+                        <div class="face face2">
+                            <h5 class="card-title"><i><a href="/Galerie/${id}">${gallery.shorttitle}</a></i></h5>
+                            <p class="gallery-description">${gallery.shortcontent}...</p>
+                            <div class="gallery-tags">`;
+
+                        gallery.tags.forEach(tag => {
+                            toAppend += ` <a href="/Galerie/Tags?t=${tag}">#${tag}</a>`;
+                        })
+                        toAppend += `   </div>
+                                        <form action="/Galerie/${id}"><button class="blog-btn">Lire plus</button></form>
+                                        </div>`;
+                        div.innerHTML = toAppend;  
+                        $("#container-gallery").append(div);
+                    }
+                });
             } else {
                 let alert = `<div id="alert" class="alert alert-warning" role="alert">
                                 <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
