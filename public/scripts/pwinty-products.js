11c72bf2f88f68d87280b2de46bafec79ff8293d
ablin <space6fic@gmail.com>
2020-03-20 05:38:00 +0100

    fetching image dimensions, added new recommended dimensions in pwinty items

--- public/scripts/pwinty-products.js
+++ public/scripts/pwinty-products.js
@@ -4,7 +4,8 @@ let PWINTY_ITEMS = {
                 "substrateType": [{"BAP" : "Budget Art Paper"}, {"BPP" : "Budget Photo Paper"}, {"CPP" : "Box Poster Paper"}, {"CPWP" : "Cold Press Watercolour Paper"}, {"EMA" : "Enhanced Matte Art Paper"},
                     {"HFAP" : "Hahnemühle Fine Art Paper"}, {"HGE" : "Hahnemühle German Etching"}, {"HPG" : "Hahnemühle Photo Glossy"}, {"HPL" : "Hahnemühle Photo Lustre"},
                     {"HPR" : "Hahnemühle Photo Rag"}, {"LPP" : "Lustre Photo Paper"}, {"MFA" : "Museum Fine Art Paper"}, {"MG" : "Metallic Gloss"}, {"SAP" : "Smooth Art Paper"}, {"SPR" : "Smooth Photo Rag"}],
-                "size": [{"20x20" : "20x20cm"}, {"20x30" : "20x30cm"}, {"A4" : "21x29.7cm"}, {"30x30" : "30x30cm"}, {"28x35" : "28x35.5cm"}, {"30x40" : "30x40cm"},
+                "size": [{"20x20" : "20x20cm", "recommendedSize" : {"2370" : "2370"}},
+                    {"20x30" : "20x30cm"}, {"A4" : "21x29.7cm"}, {"30x30" : "30x30cm"}, {"28x35" : "28x35.5cm"}, {"30x40" : "30x40cm"},
                     {"A3" : "29.7x42cm"}, {"25x50" : "25x50cm"}, {"30x45" : "30x45cm"}, {"40x40" : "40x40cm"}, {"40x50" : "40x50cm"}, {"40x60" : "40x60cm"}, {"A2" : "42x59.4cm"},
                     {"50x50" : "50x50cm"}, {"45x60" : "45x60cm"}, {"40x80" : "40x80cm"}, {"50x70" : "50x70cm"}, {"60x60" : "60x60cm"}, {"50x75" : "50x75cm"}, {"60x80" : "60x80cm"},
                     {"A1" : "59.4x84.1cm"}, {"70x70" : "70x70cm"}, {"50x100" : "50x100cm"}, {"60x90" : "60x90cm"}, {"75x75" : "75x75cm"}, {"80x80" : "80x80cm"}, {"70x100" : "70x100cm"}, {"90x90" : "90x90cm"}],
@@ -53,7 +54,9 @@ class PwintyObject {
         this.category = item.value;
         this.subcategory = ""
         this.attributes = {};
-
+        this.width = $('img[alt="0slide"]')[0].naturalWidth;
+        this.height = $('img[alt="0slide"]')[0].naturalHeight;
+        
         document.getElementById("subcategories").innerHTML = "";
         document.getElementById("attributes").innerHTML = "";
         this.hidePricing()
@@ -88,7 +91,7 @@ class PwintyObject {
                                             <option value="" disabled selected>Pick one</option>`;
             
             PWINTY_ITEMS[subcategory.dataset.category]["sharedAttributes"][attribute].forEach(selectOption => {
-                attributeSelect += `<option value="${Object.keys(selectOption)}">${Object.values(selectOption)}</option>`;
+                attributeSelect += `<option value="${Object.keys(selectOption)}">${Object.values(selectOption)[0]}</option>`;
             });
     
             attributeSelect +=              `</select>
