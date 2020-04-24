6424dffc1b85b4e46f57aeaa2ccfeb05293b1313
ablin <space6fic@gmail.com>
2020-04-24 04:08:50 +0200

    looking to remplace pwinty products object with enum and revamp code so it looks nicer and more robust

---
+++ public/scripts/pwinty-catalog.js
@@ -0,0 +1,71 @@
+let PWINTY_ITEMS = {
+    "FRA": {
+            "sharedAttributes": {
+                "substrateType": [{"BAP" : "Budget Art Paper"}, {"BPP" : "Budget Photo Paper"}, {"CPP" : "Box Poster Paper"}, {"CPWP" : "Cold Press Watercolour Paper"}, {"EMA" : "Enhanced Matte Art Paper"},
+                    {"HFAP" : "Hahnemühle Fine Art Paper"}, {"HGE" : "Hahnemühle German Etching"}, {"HPG" : "Hahnemühle Photo Glossy"}, {"HPL" : "Hahnemühle Photo Lustre"},
+                    {"HPR" : "Hahnemühle Photo Rag"}, {"LPP" : "Lustre Photo Paper"}, {"MFA" : "Museum Fine Art Paper"}, {"MG" : "Metallic Gloss"}, {"SAP" : "Smooth Art Paper"}, {"SPR" : "Smooth Photo Rag"}],
+                "size": [{"20x20" : "20x20cm"}, {"20x30" : "20x30cm"}, {"A4" : "21x29.7cm"},
+                    {"30x30" : "30x30cm"}, {"28x35" : "28x35.5cm"}, {"30x40" : "30x40cm"},
+                    {"A3" : "29.7x42cm"}, {"25x50" : "25x50cm"}, {"30x45" : "30x45cm"},
+                    {"40x40" : "40x40cm"}, {"40x50" : "40x50cm"}, {"40x60" : "40x60cm"},
+                    {"A2" : "42x59.4cm"}, {"50x50" : "50x50cm"}, {"45x60" : "45x60cm"},
+                    {"40x80" : "40x80cm"}, {"50x70" : "50x70cm"}, {"60x60" : "60x60cm"},
+                    {"50x75" : "50x75cm"}, {"60x80" : "60x80cm"},{"A1" : "59.4x84.1cm"},
+                    {"70x70" : "70x70cm"}, {"50x100" : "50x100cm"}, {"60x90" : "60x90cm"},
+                    {"75x75" : "75x75cm"}, {"80x80" : "80x80cm"}, {"70x100" : "70x100cm"},
+                    {"90x90" : "90x90cm"}],
+            },
+            "BOX": {
+                "mountType": [{"MOUNT1" : "1.4mm"}, {"MOUNT2" : "2.0mm"}, {"NM" : "NO MOUNT"}],
+                "glaze": [{"ACRY" : "Acrylic / Perspex"}, {"GLA" : "Float Glass"}, {"TRU" : "Tru View Museum Glass"}],
+                "frameColour": [{"Black" : "Black"}, {"Brown" : "Brown"}, {"White" : "White"}, {"Natural" : "Natural"}],
+                "mountColour": [{"Snow White" : "Snow White"}, {"Off-White" : "Off-White"}, {"Black" : "Black"}]
+            },
+            "CLA": {
+                "mountType": [{"MOUNT1" : "1.4mm"}, {"MOUNT2" : "2.0mm"}, {"NM" : "NO MOUNT"}],
+                "glaze": [{"ACRY" : "Acrylic / Perspex"}, {"GLA" : "Float Glass"}, {"TRU" : "Tru View Museum Glass"}],
+                "frameColour": [{"Black" : "Black"}, {"Brown" : "Brown"}, {"White" : "White"}, {"Natural" : "Natural"}, {"Silver" : "Silver"}, {"Gold" : "Gold"}],
+                "mountColour": [{"Snow White" : "Snow White"}, {"Off-White" : "Off-White"}, {"Black" : "Black"}]
+            }, 
+            "GLO": {
+                "mountType": [{"MOUNT1" : "1.4mm"}, {"MOUNT2" : "2.0mm"}, {"NM" : "NO MOUNT"}],
+                "glaze": [{"ACRY" : "Acrylic / Perspex"}], //, {"GLA" : "Float Glass"}
+                "frameColour": [{"Black" : "Black"}, {"White" : "White"}],
+                "mountColour": [{"Snow White" : "Snow White"}, {"Off-White" : "Off-White"}, {"Black" : "Black"}]
+            }, 
+            "SPACE": {
+                "glaze": [{"ACRY" : "Acrylic / Perspex"}, {"GLA" : "Float Glass"}, {"TRU" : "Tru View Museum Glass"}],
+                "frameColour": [{"Black" : "Black"}, {"Brown" : "Brown"}, {"White" : "White"}, {"Natural" : "Natural"}, {"Silver" : "Silver"}, {"Gold" : "Gold"}]
+            }, 
+            "SUR1": {
+                "frameColour": [{"Black" : "Black"}, {"White" : "White"}]
+            }, 
+            "SUR2": {
+                "frameColour": [{"Black" : "Black"}, {"White" : "White"}]
+            }, 
+            "SWO": {
+                "mountType": [{"MOUNT1" : "1.4mm"}, {"MOUNT2" : "2.0mm"}, {"NM" : "NO MOUNT"}],
+                "glaze": [{"ACRY" : "Acrylic / Perspex"}], //, {"GLA" : "Float Glass"}
+                "frameColour": [{"Black" : "Black"}, {"White" : "White"}],
+                "mountColour": [{"Snow White" : "Snow White"}, {"Off-White" : "Off-White"}, {"Black" : "Black"}]
+            },
+    },
+    "mounted": [],
+}
+
+/*
+
+
+const mountType = {
+    NM: 'No Mount',
+    MOUNT1: "1.4mm Mount",
+    MOUNT2: "2.0mm Mount"
+}
+
+const glazeType = {
+    ACRY: "Acrylic / Perspex",
+    GLA: "Float Glass",
+    TRU: "Tru View Museum Glass"
+}
+
+*/
