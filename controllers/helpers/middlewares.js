7dc34fb3b5617f299af0d802469bccb73ea4df13
ablin <space6fic@gmail.com>
2020-07-29 19:24:48 +0200

    configuring memcache

--- controllers/helpers/middlewares.js
+++ controllers/helpers/middlewares.js
@@ -175,7 +175,7 @@ function checkBilling(req, res, next) {
 
 function authToken(req, res, next) {
 	const token = req.headers.access_token;
-	console.log(token, process.env.ACCESS_TOKEN, token !== process.env.ACCESS_TOKEN);
+	console.log(token, "XXX", process.env.ACCESS_TOKEN, "XXX", token !== process.env.ACCESS_TOKEN);
 	if (!token || token !== process.env.ACCESS_TOKEN)
 		return res.status(200).json({ error: true, message: ERROR_MESSAGE.unauthorized });
 
