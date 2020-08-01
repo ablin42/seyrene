eba3973c7063f49045d9a9af07c9215141e01ab0
ablin <space6fic@gmail.com>
2020-08-02 01:01:39 +0200

    fixing nginx caching abuse

--- app.js
+++ app.js
@@ -138,9 +138,9 @@ app.use(
 		}),
 		name: "overlord",
 		secret: process.env.SESSION_SECRET,
-		resave: true,
-		saveUninitialized: true,
-		cookie: { path: "/", maxAge: 14 * 24 * 60 * 60 * 1000, httpOnly: false, secure: false }, //secure = true (or auto) requires https else it wont work
+		resave: false,
+		saveUninitialized: false,
+		cookie: { path: "/", maxAge: 14 * 24 * 60 * 60 * 1000, httpOnly: false, secure: true }, //secure = true (or auto) requires https else it wont work
 		sameSite: "Lax"
 	})
 );
