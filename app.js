b648e63cc21b4b5aba4b00ec97dad5322c1bb490
ablin <space6fic@gmail.com>
2020-08-03 00:27:47 +0200

    trying to make secure cookie work

--- app.js
+++ app.js
@@ -140,6 +140,7 @@ app.use(
 		name: "overlord",
 		secret: process.env.SESSION_SECRET,
 		resave: false,
+		key: "sid",
 		proxy: true,
 		saveUninitialized: false,
 		cookie: { path: "/", maxAge: 14 * 24 * 60 * 60 * 1000, httpOnly: false, secure: false }, //secure = true (or auto) requires https else it wont work
